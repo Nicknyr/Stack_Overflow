b9be254be6062adf8d84346ffc06696fd667aa9b
Nick <nick@Nicks-MacBook-Pro.local>
2019-08-29 02:02:10 -0400

    Aligned elements in Navbar,search input lined up correctly on lg screen views using flex property. Still needs more styling and responsive adjustments on smaller screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,37 +16,88 @@ import {
   InputGroup} from 'react-bootstrap';
 
 const NAVBAR = styled(Navbar)`
+    border-top: 3px solid #F48024;
+    border-bottom: 1px solid lightgray;
+
+    .navbar {
+
+    }
+
     .input-group {
-      width: 50%;
+      flex: 1;
+    }
+
+    .navbar-brand {
+      margin-right: 0;
+    }
+
+    .navbar-toggler {
+      border: none !important;
+      padding: 0;
     }
 `;
 
 const BUTTON = styled(Button)`
   font-size: 12px;
-  font-weight: bold;
-  background: #F48024;
-  border: 1px solid #F48024;
-  box-shadow: inset 0 1px 0 #F48024;
+  font-weight: bolder;
   border-radius: 20px;
-  padding-left: 10px;
-  padding-right: 10px;
+  padding: 6px 10px;
+  color: #535a60;
+  background: transparent;
+  border: 0px transparent;
 
   &:hover {
-    background: #F48024;
-    border: 1px solid #F48024;
-    box-shadow: inset 0 1px 0 #F48024;
+    background: #e4e6e8;
+    border: 1px solid #e4e6e8;
+    box-shadow: inset 0 1px 0 #e4e6e8;
+    color: #0C0D0E;
   }
 `;
 
-const loginButton = styled(Button)`
-  font-size: 12px;
+const ORANGEBUTTON = styled(BUTTON)`
+  background: #F48024;
+  border: 1px solid #F48024;
+  box-shadow: inset 0 1px 0 #F48024;
+  color: #FFF;
+
+  &:hover {
+    background: #da670b;
+    border: 1px solid #da670b;
+    box-shadow: inset 0 1px 0 #da670b;
+    color: #FFF;
+  }
+`;
+
+const LOGINBUTTON = styled(BUTTON)`
+  color: #07C;
+  font-weight: normal;
+
+  &:hover {
+    color: #005999;
+    background-color: rgba(0,119,204,0.1);
+    border-color: transparent;
+    box-shadow: none;
+  }
+`;
+
+const SIGNUPBUTTON = styled(BUTTON)`
+  color: #FFF;
+  background-color: #0095ff;
+  border: 1px solid #07C;
+  box-shadow: inset 0 1px 0 #66BFFF;
+
+  &:hover {
+    color: rgba(255,255,255,0.9);
+    background-color: #07C;
+    border-color: #005999;
+    box-shadow: inset 0 1px 0 #3AF;
+  }
 `;
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
-      <NAVBAR bg="light" expand="all">
+      <NAVBAR className="fixed-top justify-content-between d-flex py-0" bg="light" expand="all">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="">
@@ -68,32 +119,32 @@ class NavBar extends Component {
           </Nav>
         </Navbar.Collapse>
         <Navbar.Brand href="#home">
-          <img src={logo} alt="stack overflow logo" width="180"/>
+          <img src={logo} alt="stack overflow logo" width="160"/>
         </Navbar.Brand>
         <Nav className="mr-auto d-inline-block">
-          <Nav.Link href="#" className="d-inline">
-            <BUTTON>Products</BUTTON>
+          <Nav.Link href="#" className="d-inline mr-1">
+            <ORANGEBUTTON>Products</ORANGEBUTTON>
           </Nav.Link>
-          <Nav.Link href="#link" className="d-inline">
+          <Nav.Link href="#link" className="d-inline mr-1">
             <BUTTON>Customers</BUTTON>
           </Nav.Link>
-          <Nav.Link href="#link" className="d-inline">
+          <Nav.Link href="#link" className="d-inline mr-1">
             <BUTTON>Use cases</BUTTON>
           </Nav.Link>
         </Nav>
+        <InputGroup className="input-group-sm ml-4">
+          <FormControl
+            placeholder="Search..."
+            aria-label="Search"
+            aria-describedby="basic-addon2"
+          />
+        <InputGroup.Append className="ml-4">
+            <LOGINBUTTON variant="outline-secondary mr-1">Log in</LOGINBUTTON>
+            <SIGNUPBUTTON variant="outline-secondary">Sign up</SIGNUPBUTTON>
+          </InputGroup.Append>
+        </InputGroup>
       </NAVBAR>
-      <InputGroup>
-        <FormControl
-          placeholder="Search..."
-          aria-label="Search"
-          aria-describedby="basic-addon2"
-        />
-        <InputGroup.Append>
-          <Button variant="outline-secondary">Log in</Button>
-          <Button variant="outline-secondary">Sign up</Button>
-        </InputGroup.Append>
-      </InputGroup>
-    </div>
+
     );
   }
 }
